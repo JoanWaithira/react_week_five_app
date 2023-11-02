6e5c44cae739d8e1cf9e1fdffec2dc31bb5bc15e
Joan Waithira Njoroge <joanwaithira.jw@gmail.com>
2023-11-02 10:58:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ export default function App() {
       <h1>Weather App</h1>
 
       <footer>
-        This project is created by <a href="">Joan Waithira</a>
+        This project is created by{" "}
+        <a href="https://www.linkedin.com/in/joan-waithira/">Joan Waithira</a>
         {""} and is hosted on {""}
         <a href="https://github.com/JoanWaithira/react_week_five_app">Github</a>
       </footer>
