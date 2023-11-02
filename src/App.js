e7c036ee33b592a6cf4f9af3faa9bd6b8b2d3efd
Joan Waithira Njoroge <joanwaithira.jw@gmail.com>
2023-11-02 11:29:42 +0300

    I have added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project is created by{" "}
-        <a href="https://www.linkedin.com/in/joan-waithira/">Joan Waithira</a>
-        {""} and is hosted on {""}
-        <a href="https://github.com/JoanWaithira/react_week_five_app">Github</a>
-      </footer>
+        <footer>
+          This project is created by{" "}
+          <a href="https://www.linkedin.com/in/joan-waithira/">Joan Waithira</a>
+          {""} and is hosted on {""}
+          <a href="https://github.com/JoanWaithira/react_week_five_app">
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
