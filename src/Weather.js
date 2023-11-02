8d736007ed5028235e8c19cdf8e930beb86a58fe
Joan Waithira Njoroge <joanwaithira.jw@gmail.com>
2023-11-02 20:42:26 +0300

    Few Changes

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({});
   function handleResponse(response) {
@@ -66,7 +66,7 @@ export default function Weather() {
           <div className="col-6">
             <ul>
               {/* <li>Precipitation: 15%</li> */}
-              <li>Humidity: {weatherData.humidity}</li>
+              <li>Humidity: {weatherData.humidity}%</li>
               <li>Wind: {weatherData.wind}km/h</li>
             </ul>
           </div>
@@ -75,8 +75,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "8b07e12d571f0d5de5890ce3550e06cf";
-    let city = "New York";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    // let city = "New York";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading The Weather App....";
