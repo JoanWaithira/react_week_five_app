ea62a229449d19d384058df91ed35afe9f55a932
Joan Waithira Njoroge <joanwaithira.jw@gmail.com>
2023-11-02 22:44:48 +0300

    Still working on the search engine

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 // import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature";
+// import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="WeatherInfo">
+    <div className="Weatherinfo">
       <h1>{props.data.city}</h1>
       <ul>
         <li>
@@ -15,21 +15,25 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
-              {/* <WeatherIcon code={props.data.icon} size={52} /> */}
-            </div>
-
-            <div className="float-left">
-              {Math.round(props.data.Temperature)}
-              {/* <WeatherTemperature celsius={props.data.temperature} /> */}
-            </div>
-          </div>
+          {/* <div className="clearfix mt-3"> */}
+          <img
+            src={props.data.icon}
+            alt={props.data.description}
+            className="float-left mt-3"
+          />
+          {/* </div> */}
+          {/* <div className="float-left"> */}
+          <span className="temperature">
+            {Math.round(props.data.temperature)}
+          </span>
+          <span className="unit">°C</span>
+          {/* </div> */}
         </div>
         <div className="col-6">
           <ul>
+            {/* <li>Precipitation: 15%</li> */}
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Wind: {props.data.wind}km/h</li>
           </ul>
         </div>
       </div>
